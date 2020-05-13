44ddab94c10848ea4b9991ec6d62f67ce963aab7
dbuckstein <dbuckstein@users.noreply.github.com>
2020-05-13 14:18:09 -0400

    Fixed up typedefs to easily convert type byte sizes to chunk sizes. MODIFIED: typedefs header.

--- ijk/include/ijk/ijk/ijk-typedefs.h
+++ ijk/include/ijk/ijk/ijk-typedefs.h
@@ -42,70 +42,77 @@ typedef	unsigned	long					ui32, dword;
 typedef	unsigned	long long				ui64, qword;
 typedef				float					f32, flt;
 typedef				double					f64, dbl;
-#define				szbyte					((size)sizeof(byte))
-#define				szword					((size)sizeof(word))
-#define				szdword					((size)sizeof(dword))
-#define				szqword					((size)sizeof(qword))
-#define				szflt					((size)sizeof(flt))
-#define				szdbl					((size)sizeof(dbl))
 
 
 // core pointer type abbreviations
 ///
 typedef				void					* ptr, * addr;
 typedef				void const				* kptr, * kaddr;
-#define				szaddr					((size)sizeof(addr))
 
 
 // convenient types sized for current architecture
 ///
 typedef				ijk_tokencat_arch( i)	flag, ptrdiff, schomp, itr, index;
 typedef				ijk_tokencat_arch(ui)	size, chomp, uchomp, uitr, uindex;
-#define				szchomp					((size)sizeof(chomp))
+
+
+// convenient size abbreviations for byte and chomp
+///
+#define				szb(x)					(size)sizeof(x)
+#define				szbyte					szb(byte)
+#define				szchomp					szb(chomp)
+#define				ijk_b2c(b)				(((b)+szchomp-1)/szchomp)
+#define				ijk_c2b(c)				((c)*szchomp)
+#define				szc(x)					ijk_b2c(szb(x))
+
+
+// convenient size abbreviations for other types
+///
+#define				szword					szb(word)
+#define				szdword					szb(dword)
+#define				szqword					szb(qword)
+#define				szflt					szb(flt)
+#define				szdbl					szb(dbl)
+#define				szaddr					szb(addr)
 
 
 // convenient short c-string type abbreviations
 ///
 typedef				byte					tag[szdword * szqword], * ptag;
 typedef				byte const				* kptag, * kpbyte, * kcstr;
-#define				sztag					((size)sizeof(tag))
+#define				sztag					szb(tag)
 
 
 // convenient short wide-character string type abbreviations
 ///
 typedef				word					wtag[sztag], * pwtag, * wcstr;
 typedef				word const				* kpwtag, * kwcstr;
-#define				szwtag					((size)sizeof(wtag))
+#define				szwtag					szb(wtag)
 
 
 // convenient short dynamic (architecture-dependent) c-string abbreviations
 ///
 typedef				byte					dtag[szchomp * szqword], * pdtag;
 typedef				byte const				* kdtag;
-#define				szdtag					((size)sizeof(dtag))
+#define				szdtag					szb(dtag)
 
 
 // convenient short dynamic wide-character string abbreviation
 ///
 typedef				word					dwtag[szdtag], *pdwtag;
 typedef				word const				* kdwtag;
-#define				szdwtag					((size)sizeof(dwtag))
+#define				szdwtag					szb(dwtag)
 
 
 // simple copy operations
 ///
-#define ijk_copyb(dst, src, offset)		(*((byte*)(dst) + (offset)) = *((byte const *)(src) + (offset)))
-#define ijk_copyw(dst, src, offset)		(*((word*)(dst) + (offset)) = *((word const *)(src) + (offset)))
-#define ijk_copydw(dst, src, offset)	(*((dword*)(dst) + (offset)) = *((dword const*)(src) + (offset)))
-#define ijk_copyqw(dst, src, offset)	(*((qword*)(dst) + (offset)) = *((qword const*)(src) + (offset)))
-#define ijk_copytag(dst, src)			ijk_copyqw(dst,src,0);ijk_copyqw(dst,src,1);ijk_copyqw(dst,src,2);ijk_copyqw(dst,src,3)
-#define ijk_copywtag(dst, src)			ijk_copytag((ptag)(dst),(kptag)(src));ijk_copytag((ptag)(dst)+sztag,(kptag)(src)+sztag)
-
-
-// conversion between bytes/chomps
-///
-#define ijk_byte2chomp(bytes)			((bytes+szchomp-1)/szchomp)
-#define ijk_chomp2byte(chomps)			(chomps*szchomp)
+#define ijk_copyb(dst, src, offset)			(*((byte*)(dst) + (offset)) = *((byte const *)(src) + (offset)))
+#define ijk_copyw(dst, src, offset)			(*((word*)(dst) + (offset)) = *((word const *)(src) + (offset)))
+#define ijk_copydw(dst, src, offset)		(*((dword*)(dst) + (offset)) = *((dword const*)(src) + (offset)))
+#define ijk_copyqw(dst, src, offset)		(*((qword*)(dst) + (offset)) = *((qword const*)(src) + (offset)))
+#define ijk_copyc(dst, src, offset)			(*((chomp*)(dst) + (offset)) = *((chomp const*)(src) + (offset)))
+#define ijk_copytag(dst, src)				ijk_copyqw(dst,src,0);ijk_copyqw(dst,src,1);ijk_copyqw(dst,src,2);ijk_copyqw(dst,src,3)
+#define ijk_copywtag(dst, src)				ijk_copytag((ptag)(dst),(kptag)(src));ijk_copytag((ptag)(dst)+sztag,(kptag)(src)+sztag)
 
 
 #endif	// !_IJK_TYPEDEFS_H_
