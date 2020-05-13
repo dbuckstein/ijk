a0fe36d79d3bbdce302f2e4c77a7585c8a859aa8
dbuckstein <dbuckstein@users.noreply.github.com>
2020-05-13 16:47:59 -0400

    Added remaining function documentation. MODIFIED: memory header.

--- ijk/include/ijk/ijk-base/ijk-utility/ijkMemory.h
+++ ijk/include/ijk/ijk-base/ijk-utility/ijkMemory.h
@@ -175,29 +175,88 @@ typedef ptr(*ijkMemoryInitCallback)(ptr dst, size sz_bytes);
 //		return FAILURE: ijk_fail_invalidparams if invalid parameters
 //		return FAILURE: ijk_fail_operationfail if pool not saved
 
+// ijkMemoryPoolDefragment
+//	Defragment pool.
+//		param pool: base pointer to pre-allocated block
+//			valid: non-null, initialized
+//		return SUCCESS: ijk_success if defragmented
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if not defragmented
+
 // ijkMemoryPoolGetName
 //	Get the name of a pool.
+//		param pool: base pointer to pre-allocated block
+//			valid: non-null, initialized
+//		param name_out: name of pool to be retrieved
+//			valid: non-null c-string
+//		return SUCCESS: ijk_success if name retrieved
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if name not retrieved
 
 // ijkMemoryPoolSetName
 //	Set the name of a pool.
+//		param pool: base pointer to pre-allocated block
+//			valid: non-null, initialized
+//		param name: name of pool to be set
+//			valid: non-null, non-empty c-string
+//		return SUCCESS: ijk_success if name set
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if name not set
 
 // ijkMemoryPoolGetReserved
 //	Get the number of bytes reserved.
+//		param pool: base pointer to pre-allocated block
+//			valid: non-null, initialized
+//		param size_out: pointer to reserved size
+//			valid: non-null
+//			note: upon function success, points to non-negative reserved size
+//		return SUCCESS: ijk_success if size retrieved
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if size not retrieved
 
 // ijkMemoryPoolGetAvailable
 //	Get the number of bytes available.
+//		param pool: base pointer to pre-allocated block
+//			valid: non-null, initialized
+//		param size_out: pointer to available size
+//			valid: non-null
+//			note: upon function success, points to non-negative available size
+//		return SUCCESS: ijk_success if size retrieved
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if size not retrieved
 
 // ijkMemoryPoolGetFragmented
 //	Get the number of bytes fragmented.
+//		param pool: base pointer to pre-allocated block
+//			valid: non-null, initialized
+//		param size_out: pointer to fragmented size
+//			valid: non-null
+//			note: upon function success, points to non-negative fragmented size
+//		return SUCCESS: ijk_success if size retrieved
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if size not retrieved
 
 // ijkMemoryPoolGetSize
 //	Get the full size of a pool in bytes.
+//		param pool: base pointer to pre-allocated block
+//			valid: non-null, initialized
+//		param size_out: pointer to size of pool
+//			valid: non-null
+//			note: upon function success, points to non-negative pool size
+//		return SUCCESS: ijk_success if size retrieved
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if size not retrieved
 
 // ijkMemoryPoolGetBlock
 //	Find a reserved block by name within a pool.
-
-// ijkMemoryPoolDefragment
-//	Defragment pool.
+//		param pool: base pointer to pre-allocated block
+//			valid: non-null, initialized
+//		param block_out: pointer to block pointer
+//			valid: non-null, points to null
+//			note: upon function success, points to non-null block address
+//		return SUCCESS: ijk_success if block retrieved
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if block not retrieved
 
 
 //-----------------------------------------------------------------------------
@@ -212,6 +271,8 @@ typedef ptr(*ijkMemoryInitCallback)(ptr dst, size sz_bytes);
 //			valid: non-zero
 //		param name: name of pool
 //			valid: non-zero, non-empty c-string
+//		param type: user-defined data type of block
+//			valid: non-negative
 //		param initCallback_opt: optional initialization callback
 //		return SUCCESS: ijk_success if block reserved
 //		return FAILURE: ijk_fail_invalidparams if invalid parameters
@@ -255,21 +316,66 @@ typedef ptr(*ijkMemoryInitCallback)(ptr dst, size sz_bytes);
 
 // ijkMemoryBlockGetName
 //	Get the name of a reserved block.
+//		param block: pointer to block
+//			valid: non-null, initialized
+//		param name_out: name of pool to be retrieved
+//			valid: non-null c-string
+//		return SUCCESS: ijk_success if name retrieved
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if name not retrieved
 
 // ijkMemoryBlockSetName
 //	Set the name of a reserved block.
+//		param block: pointer to block
+//			valid: non-null, initialized
+//		param name: name of pool to be set
+//			valid: non-null, non-empty c-string
+//		return SUCCESS: ijk_success if name set
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if name not set
 
 // ijkMemoryBlockGetType
 //	Get the user-defined data type of a reserved block.
+//		param block: pointer to block
+//			valid: non-null, initialized
+//		param type_out: pointer to user-defined data type of block
+//			valid: non-null
+//			note: upon function success, points to non-negative type identifier
+//		return SUCCESS: ijk_success if type retrieved
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if type not retrieved
 
 // ijkMemoryBlockSetType
 //	Set the user-defined data type of a reserved block.
+//		param block: pointer to block
+//			valid: non-null, initialized
+//		param type: user-defined data type of block
+//			valid: non-negative
+//		return SUCCESS: ijk_success if type set
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if type not set
 
 // ijkMemoryBlockGetSize
 //	Get the size of a reserved block.
+//		param block: pointer to block
+//			valid: non-null, initialized
+//		param size_out: pointer to size of block
+//			valid: non-null
+//			note: upon function success, points to non-negative block size
+//		return SUCCESS: ijk_success if size retrieved
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if size not retrieved
 
 // ijkMemoryBlockIsInPool
 //	Check if reserved block is ontained within a pool.
+//		param block: pointer to block
+//			valid: non-null, initialized
+//		param status_out: pointer to status
+//			valid: non-null
+//			note: upon function success, points to boolean status
+//		return SUCCESS: ijk_success if status retrieved
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+//		return FAILURE: ijk_fail_operationfail if status not retrieved
 
 
 //-----------------------------------------------------------------------------
