ddec5a4219329d9d8230dd476904c6520634e5c9
dbuckstein <dbuckstein@users.noreply.github.com>
2020-05-25 00:57:01 -0400

    Adding prototypes for gamepad. MODIFIED: gamepad header.

--- ijk/include/ijk/ijk-base/ijk-input/ijkGamepad.h
+++ ijk/include/ijk/ijk-base/ijk-input/ijkGamepad.h
@@ -33,12 +33,24 @@
 #ifdef __cplusplus
 extern "C" {
 #else	// !__cplusplus
+typedef enum ijkGamepadID				ijkGamepadID;
 typedef enum ijkGamepadBtn				ijkGamepadBtn;
 typedef struct ijkGamepadState			ijkGamepadState;
 #endif	// __cplusplus
 
 
 //-----------------------------------------------------------------------------
+
+// ijkGamepadID
+//	Enumeration of gamepad IDs by player number.
+enum ijkGamepadID
+{
+	ijkGamepadID_player1,
+	ijkGamepadID_player2,
+	ijkGamepadID_player3,
+	ijkGamepadID_player4,
+};
+
 
 // ijkGamepadBtn
 //	Enumeration of gamepad buttons.
@@ -92,6 +104,102 @@ struct ijkGamepadState
 
 //-----------------------------------------------------------------------------
 
+// ijkGamepadGetButtonState
+//	Get the current state of a gamepad button.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param state_out: pointer to button state
+//			valid: non-null
+//			note: upon successful return, points to boolean (true is pressed)
+//		param button: gamepad button
+//		return SUCCESS: ijk_success if retrieved state
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadGetButtonState(ijkGamepadState const* const gamepad, ibool state_out[1], ijkGamepadBtn const button);
+
+// ijkGamepadIsButtonDown
+//	Check whether gamepad button is down.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param button: button enumerator
+//		return SUCCESS: ijk_true if key is down
+//		return SUCCESS: ijk_false if key is up
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadIsButtonDown(ijkGamepadState const* const gamepad, ijkGamepadBtn const button);
+
+// ijkGamepadIsButtonUp
+//	Check whether gamepad button is up.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param button: button enumerator
+//		return SUCCESS: ijk_true if key is up
+//		return SUCCESS: ijk_false if key is down
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadIsButtonUp(ijkGamepadState const* const gamepad, ijkGamepadBtn const button);
+
+// ijkGamepadIsButtonDownAgain
+//	Check whether gamepad button is consistently down between updates.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param button: button enumerator
+//		return SUCCESS: ijk_true if key is consistently down
+//		return SUCCESS: ijk_false if key is not consistently down
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadIsButtonDownAgain(ijkGamepadState const* const gamepad, ijkGamepadBtn const button);
+
+// ijkGamepadIsButtonUpAgain
+//	Check whether gamepad button is consistently up between updates.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param button: button enumerator
+//		return SUCCESS: ijk_true if key is consistently up
+//		return SUCCESS: ijk_false if key is not consistently up
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadIsButtonUpAgain(ijkGamepadState const* const gamepad, ijkGamepadBtn const button);
+
+// ijkGamepadIsButtonPressed
+//	Check whether gamepad button changed from up to down.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param button: button enumerator
+//		return SUCCESS: ijk_true if key changed from up to down
+//		return SUCCESS: ijk_false if key did not change from up to down
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadIsButtonPressed(ijkGamepadState const* const gamepad, ijkGamepadBtn const button);
+
+// ijkGamepadIsButtonReleased
+//	Check whether gamepad button changed from down to up.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param button: button enumerator
+//		return SUCCESS: ijk_true if key changed from down to up
+//		return SUCCESS: ijk_false if key did not change from down to up
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadIsButtonReleased(ijkGamepadState const* const gamepad, ijkGamepadBtn const button);
+
+// ijkGamepadUpdate
+//	Copy the current state to the previous state.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		return SUCCESS: ijk_success if state updated
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadUpdate(ijkGamepadState* const gamepad);
+
+// ijkGamepadReset
+//	Reset the current state.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		return SUCCESS: ijk_success if state reset
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadReset(ijkGamepadState* const gamepad);
+
+// ijkGamepadSetID
+//	Initialize gamepad given ID and update.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param gamepadID: gamepad player identifier
+//		return SUCCESS: ijk_success if ID set
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadSetID(ijkGamepadState* const gamepad, ijkGamepadID const gamepadID);
 
 
 //-----------------------------------------------------------------------------
