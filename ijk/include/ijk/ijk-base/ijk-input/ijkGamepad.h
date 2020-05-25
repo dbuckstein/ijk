7b4f407edc85181656cb82e8a073037f759991ef
dbuckstein <dbuckstein@users.noreply.github.com>
2020-05-25 01:24:51 -0400

    Added remaining gamepad prototypes and comments. MODIFIED: gamepad header.

--- ijk/include/ijk/ijk-base/ijk-input/ijkGamepad.h
+++ ijk/include/ijk/ijk-base/ijk-input/ijkGamepad.h
@@ -121,8 +121,8 @@ iret ijkGamepadGetButtonState(ijkGamepadState const* const gamepad, ibool state_
 //		param gamepad: pointer to gamepad descriptor
 //			valid: non-null
 //		param button: button enumerator
-//		return SUCCESS: ijk_true if key is down
-//		return SUCCESS: ijk_false if key is up
+//		return SUCCESS: ijk_true if button is down
+//		return SUCCESS: ijk_false if button is up
 //		return FAILURE: ijk_fail_invalidparams if invalid parameters
 iret ijkGamepadIsButtonDown(ijkGamepadState const* const gamepad, ijkGamepadBtn const button);
 
@@ -131,8 +131,8 @@ iret ijkGamepadIsButtonDown(ijkGamepadState const* const gamepad, ijkGamepadBtn 
 //		param gamepad: pointer to gamepad descriptor
 //			valid: non-null
 //		param button: button enumerator
-//		return SUCCESS: ijk_true if key is up
-//		return SUCCESS: ijk_false if key is down
+//		return SUCCESS: ijk_true if button is up
+//		return SUCCESS: ijk_false if button is down
 //		return FAILURE: ijk_fail_invalidparams if invalid parameters
 iret ijkGamepadIsButtonUp(ijkGamepadState const* const gamepad, ijkGamepadBtn const button);
 
@@ -141,8 +141,8 @@ iret ijkGamepadIsButtonUp(ijkGamepadState const* const gamepad, ijkGamepadBtn co
 //		param gamepad: pointer to gamepad descriptor
 //			valid: non-null
 //		param button: button enumerator
-//		return SUCCESS: ijk_true if key is consistently down
-//		return SUCCESS: ijk_false if key is not consistently down
+//		return SUCCESS: ijk_true if button is consistently down
+//		return SUCCESS: ijk_false if button is not consistently down
 //		return FAILURE: ijk_fail_invalidparams if invalid parameters
 iret ijkGamepadIsButtonDownAgain(ijkGamepadState const* const gamepad, ijkGamepadBtn const button);
 
@@ -151,8 +151,8 @@ iret ijkGamepadIsButtonDownAgain(ijkGamepadState const* const gamepad, ijkGamepa
 //		param gamepad: pointer to gamepad descriptor
 //			valid: non-null
 //		param button: button enumerator
-//		return SUCCESS: ijk_true if key is consistently up
-//		return SUCCESS: ijk_false if key is not consistently up
+//		return SUCCESS: ijk_true if button is consistently up
+//		return SUCCESS: ijk_false if button is not consistently up
 //		return FAILURE: ijk_fail_invalidparams if invalid parameters
 iret ijkGamepadIsButtonUpAgain(ijkGamepadState const* const gamepad, ijkGamepadBtn const button);
 
@@ -161,8 +161,8 @@ iret ijkGamepadIsButtonUpAgain(ijkGamepadState const* const gamepad, ijkGamepadB
 //		param gamepad: pointer to gamepad descriptor
 //			valid: non-null
 //		param button: button enumerator
-//		return SUCCESS: ijk_true if key changed from up to down
-//		return SUCCESS: ijk_false if key did not change from up to down
+//		return SUCCESS: ijk_true if button changed from up to down
+//		return SUCCESS: ijk_false if button did not change from up to down
 //		return FAILURE: ijk_fail_invalidparams if invalid parameters
 iret ijkGamepadIsButtonPressed(ijkGamepadState const* const gamepad, ijkGamepadBtn const button);
 
@@ -171,16 +171,151 @@ iret ijkGamepadIsButtonPressed(ijkGamepadState const* const gamepad, ijkGamepadB
 //		param gamepad: pointer to gamepad descriptor
 //			valid: non-null
 //		param button: button enumerator
-//		return SUCCESS: ijk_true if key changed from down to up
-//		return SUCCESS: ijk_false if key did not change from down to up
+//		return SUCCESS: ijk_true if button changed from down to up
+//		return SUCCESS: ijk_false if button did not change from down to up
 //		return FAILURE: ijk_fail_invalidparams if invalid parameters
 iret ijkGamepadIsButtonReleased(ijkGamepadState const* const gamepad, ijkGamepadBtn const button);
+
+// ijkGamepadIsConnected
+//	Check whether gamepad is connected.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		return SUCCESS: ijk_true if gamepad is connected
+//		return SUCCESS: ijk_false if gamepad is not connected
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadIsConnected(ijkGamepadState const* const gamepad);
+
+// ijkGamepadIsNotConnected
+//	Check whether gamepad is not connected.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		return SUCCESS: ijk_true if gamepad is not connected
+//		return SUCCESS: ijk_false if gamepad is connected
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadIsNotConnected(ijkGamepadState const* const gamepad);
+
+// ijkGamepadIsConnectedAgain
+//	Check whether gamepad is consistently connected between updates.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		return SUCCESS: ijk_true if gamepad is consistently connected
+//		return SUCCESS: ijk_false if gamepad is not consistently connected
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadIsConnectedAgain(ijkGamepadState const* const gamepad);
+
+// ijkGamepadIsNotConnectedAgain
+//	Check whether gamepad is consistently not connected between updates.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		return SUCCESS: ijk_true if gamepad is consistently not connected
+//		return SUCCESS: ijk_false if gamepad is not consistently not connected
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadIsNotConnectedAgain(ijkGamepadState const* const gamepad);
+
+// ijkGamepadReconnected
+//	Check whether gamepad reconnected since last update.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		return SUCCESS: ijk_true if gamepad changed to connected
+//		return SUCCESS: ijk_false if gamepad did not change to connected
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadReconnected(ijkGamepadState const* const gamepad);
+
+// ijkGamepadDisconnected
+//	Check whether gamepad disconnected since last update.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		return SUCCESS: ijk_true if gamepad changed to not connected
+//		return SUCCESS: ijk_false if gamepad did not change to not connected
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadDisconnected(ijkGamepadState const* const gamepad);
+
+// ijkGamepadGetTriggerLeft
+//	Get the left trigger as a unit value.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param value_out: pointer to trigger value output
+//			valid: non-null
+//		return SUCCESS: ijk_success if retrieved value
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadGetTriggerLeft(ijkGamepadState const* const gamepad, dbl value_out[1]);
+
+// ijkGamepadGetTriggerRight
+//	Get the right trigger as a unit value.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param value_out: pointer to trigger value output
+//			valid: non-null
+//		return SUCCESS: ijk_success if retrieved value
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadGetTriggerRight(ijkGamepadState const* const gamepad, dbl value_out[1]);
+
+// ijkGamepadGetTriggerLeftChange
+//	Get the left trigger value difference from the previous state.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param dv_out: pointer to trigger value output
+//			valid: non-null
+//		return SUCCESS: ijk_success if retrieved value
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadGetTriggerLeftChange(ijkGamepadState const* const gamepad, dbl dv_out[1]);
+
+// ijkGamepadGetTriggerRightChange
+//	Get the right trigger value difference from the previous state.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param dv_out: pointer to trigger value output
+//			valid: non-null
+//		return SUCCESS: ijk_success if retrieved value
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadGetTriggerRightChange(ijkGamepadState const* const gamepad, dbl dv_out[1]);
+
+// ijkGamepadGetThumbstickLeft
+//	Get the left thumbstick as a unit direction vector.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param vector_out: pointer to joystick vector output
+//			valid: non-null
+//		return SUCCESS: ijk_success if retrieved value
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadGetThumbstickLeft(ijkGamepadState const* const gamepad, dbl vector_out[1]);
+
+// ijkGamepadGetThumbstickRight
+//	Get the right thumbstick as a unit direction vector.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param vector_out: pointer to joystick vector output
+//			valid: non-null
+//		return SUCCESS: ijk_success if retrieved value
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadGetThumbstickRight(ijkGamepadState const* const gamepad, dbl vector_out[1]);
+
+// ijkGamepadGetThumbstickLeftChange
+//	Get the left thumbstick vector difference from the previous state.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param dv_out: pointer to joystick vector output
+//			valid: non-null
+//		return SUCCESS: ijk_success if retrieved value
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadGetThumbstickLeftChange(ijkGamepadState const* const gamepad, dbl dv_out[1]);
+
+// ijkGamepadGetThumbstickRightChange
+//	Get the right thumbstick vector difference from the previous state.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param dv_out: pointer to joystick vector output
+//			valid: non-null
+//		return SUCCESS: ijk_success if retrieved value
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadGetThumbstickRightChange(ijkGamepadState const* const gamepad, dbl dv_out[1]);
 
 // ijkGamepadUpdate
 //	Copy the current state to the previous state.
 //		param gamepad: pointer to gamepad descriptor
 //			valid: non-null
 //		return SUCCESS: ijk_success if state updated
+//		return FAILURE: ijk_fail_operationfail if state not updated
 //		return FAILURE: ijk_fail_invalidparams if invalid parameters
 iret ijkGamepadUpdate(ijkGamepadState* const gamepad);
 
@@ -200,6 +335,17 @@ iret ijkGamepadReset(ijkGamepadState* const gamepad);
 //		return SUCCESS: ijk_success if ID set
 //		return FAILURE: ijk_fail_invalidparams if invalid parameters
 iret ijkGamepadSetID(ijkGamepadState* const gamepad, ijkGamepadID const gamepadID);
+
+// ijkGamepadSetRumble
+//	Set gamepad rumble motors.
+//		param gamepad: pointer to gamepad descriptor
+//			valid: non-null
+//		param rumble_left: new setting for left motor
+//		param rumble_right: new setting for right motor
+//		return SUCCESS: ijk_success if rumble set
+//		return FAILURE: ijk_fail_operationfail if did not set rumble
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkGamepadSetRumble(ijkGamepadState* const gamepad, word const rumble_left, word const rumble_right);
 
 
 //-----------------------------------------------------------------------------
