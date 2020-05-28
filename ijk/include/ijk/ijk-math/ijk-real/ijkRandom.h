5965c313fee3e6da6a7dd2b180903dfa9f9902ce
dbuckstein <dbuckstein@users.noreply.github.com>
2020-05-28 15:18:58 -0400

    Declaring the next set of random number generator prototypes. MODIFIED: random header.

--- ijk/include/ijk/ijk-math/ijk-real/ijkRandom.h
+++ ijk/include/ijk/ijk-math/ijk-real/ijkRandom.h
@@ -63,6 +63,65 @@ flt ijkRandomNum_flt();
 //		return: random number
 dbl ijkRandomNum_dbl();
 
+// ijkRandomNumMax_int
+//	Generate a random integer with specified maximum in [0, usermax).
+//		param usermax: user-specified maximum
+//		return: random number
+i32 ijkRandomNumMax_int(i32 const usermax);
+
+// ijkRandomNumMax_flt
+//	Generate a random float with specified maximum in [0, usermax).
+//		param usermax: user-specified maximum
+//		return: random number
+flt ijkRandomNumMax_flt(flt const usermax);
+
+// ijkRandomNumMax_dbl
+//	Generate a random double with specified maximum in [0, usermax).
+//		param usermax: user-specified maximum
+//		return: random number
+dbl ijkRandomNumMax_dbl(dbl const usermax);
+
+// ijkRandomNumRange_int
+//	Generate a random integer with specified range in [usermin, usermax).
+//		param usermin: user-specified minimum
+//		param usermax: user-specified maximum
+//		return: random number
+i32 ijkRandomNumRange_i32(i32 const usermin, i32 const usermax);
+
+// ijkRandomNumRange_flt
+//	Generate a random float with specified range in [usermin, usermax).
+//		param usermin: user-specified minimum
+//		param usermax: user-specified maximum
+//		return: random number
+flt ijkRandomNumRange_flt(flt const usermin, flt const usermax);
+
+// ijkRandomNumRange_dbl
+//	Generate a random double with specified range in [usermin, usermax).
+//		param usermin: user-specified minimum
+//		param usermax: user-specified maximum
+//		return: random number
+dbl ijkRandomNumRange_dbl(dbl const usermin, dbl const usermax);
+
+// ijkRandomNumUnitNrm_flt
+//	Generate a random float in normal unit range [0, 1).
+//		return random number
+flt ijkRandomNumUnitNrm_flt();
+
+// ijkRandomNumUnitNrm_dbl
+//	Generate a random double in normal unit range [0, 1).
+//		return random number
+dbl ijkRandomNumUnitNrm_dbl();
+
+// ijkRandomNumUnitSym_flt
+//	Generate a random float in symmetric unit range [-1, +1).
+//		return random number
+flt ijkRandomNumUnitSym_flt();
+
+// ijkRandomNumUnitSym_dbl
+//	Generate a random float in symmetric unit range [-1, +1).
+//		return random number
+dbl ijkRandomNumUnitSym_dbl();
+
 
 //-----------------------------------------------------------------------------
 
