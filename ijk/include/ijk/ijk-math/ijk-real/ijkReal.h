009e4cfeaaa9e33d349a577e1f0eb493c71347b4
dbuckstein <dbuckstein@users.noreply.github.com>
2020-05-28 18:28:29 -0400

    Added real source to define constant real values and fix the real macros. MODIFIED: math project and filters, added source; real header, reorganized constants, only using macros to define real from either float or double constants. ADDED: real source with constants.

--- ijk/include/ijk/ijk-math/ijk-real/ijkReal.h
+++ ijk/include/ijk/ijk-math/ijk-real/ijkReal.h
@@ -37,21 +37,171 @@
 //	defined, the base real number type defaults to float.	
 #ifdef IJK_REAL_DBL
 // Real number type is based on 64-bit float (double).
-#define ijk_real		dbl
+#define ijk_real					dbl
 #else	// !IJK_REAL_DBL
 // Real number type is based on 32-bit float (single).
-#define ijk_real		flt
+#define ijk_real					flt
 #endif	// IJK_REAL_DBL
 
 // real
 //	Real number type.
-typedef ijk_real		real;
+typedef ijk_real					real;
 
 // ijk_x2real
 //	Cast to real.
 //		param x: value to cast
 //		return: value as real
-#define ijk_x2real(x)	(real)(x)
+#define ijk_x2real(x)				(real)(x)
+
+
+// global constants for float type
+///
+ijk_ext flt const flt_zero;
+ijk_ext flt const flt_eps;
+ijk_ext flt const flt_halfeps;
+ijk_ext flt const flt_1000th;
+ijk_ext flt const flt_100th;
+ijk_ext flt const flt_10th;
+ijk_ext flt const flt_9th;
+ijk_ext flt const flt_8th;
+ijk_ext flt const flt_7th;
+ijk_ext flt const flt_6th;
+ijk_ext flt const flt_5th;
+ijk_ext flt const flt_quarter;
+ijk_ext flt const flt_third;
+ijk_ext flt const flt_half;
+ijk_ext flt const flt_2thirds;
+ijk_ext flt const flt_3quarters;
+ijk_ext flt const flt_one;
+ijk_ext flt const flt_4third;
+ijk_ext flt const flt_3half;
+ijk_ext flt const flt_two;
+ijk_ext flt const flt_three;
+ijk_ext flt const flt_four;
+ijk_ext flt const flt_five;
+ijk_ext flt const flt_six;
+ijk_ext flt const flt_seven;
+ijk_ext flt const flt_eight;
+ijk_ext flt const flt_nine;
+ijk_ext flt const flt_ten;
+ijk_ext flt const flt_hundred;
+ijk_ext flt const flt_thousand;
+ijk_ext flt const flt_sqrtthird;
+ijk_ext flt const flt_sqrthalf;
+ijk_ext flt const flt_sqrt2;
+ijk_ext flt const flt_sqrt3;
+ijk_ext flt const flt_2pi;
+ijk_ext flt const flt_pi;
+ijk_ext flt const flt_halfpi;
+ijk_ext flt const flt_thirdpi;
+ijk_ext flt const flt_quarterpi;
+ijk_ext flt const flt_360;
+ijk_ext flt const flt_180;
+ijk_ext flt const flt_90;
+ijk_ext flt const flt_60;
+ijk_ext flt const flt_45;
+ijk_ext flt const flt_deg2rad;
+ijk_ext flt const flt_rad2deg;
+
+
+// global constants for double type
+///
+ijk_ext dbl const dbl_zero;
+ijk_ext dbl const dbl_eps;
+ijk_ext dbl const dbl_halfeps;
+ijk_ext dbl const dbl_1000th;
+ijk_ext dbl const dbl_100th;
+ijk_ext dbl const dbl_10th;
+ijk_ext dbl const dbl_9th;
+ijk_ext dbl const dbl_8th;
+ijk_ext dbl const dbl_7th;
+ijk_ext dbl const dbl_6th;
+ijk_ext dbl const dbl_5th;
+ijk_ext dbl const dbl_quarter;
+ijk_ext dbl const dbl_third;
+ijk_ext dbl const dbl_half;
+ijk_ext dbl const dbl_2thirds;
+ijk_ext dbl const dbl_3quarters;
+ijk_ext dbl const dbl_one;
+ijk_ext dbl const dbl_4third;
+ijk_ext dbl const dbl_3half;
+ijk_ext dbl const dbl_two;
+ijk_ext dbl const dbl_three;
+ijk_ext dbl const dbl_four;
+ijk_ext dbl const dbl_five;
+ijk_ext dbl const dbl_six;
+ijk_ext dbl const dbl_seven;
+ijk_ext dbl const dbl_eight;
+ijk_ext dbl const dbl_nine;
+ijk_ext dbl const dbl_ten;
+ijk_ext dbl const dbl_hundred;
+ijk_ext dbl const dbl_thousand;
+ijk_ext dbl const dbl_sqrtthird;
+ijk_ext dbl const dbl_sqrthalf;
+ijk_ext dbl const dbl_sqrt2;
+ijk_ext dbl const dbl_sqrt3;
+ijk_ext dbl const dbl_2pi;
+ijk_ext dbl const dbl_pi;
+ijk_ext dbl const dbl_halfpi;
+ijk_ext dbl const dbl_thirdpi;
+ijk_ext dbl const dbl_quarterpi;
+ijk_ext dbl const dbl_360;
+ijk_ext dbl const dbl_180;
+ijk_ext dbl const dbl_90;
+ijk_ext dbl const dbl_60;
+ijk_ext dbl const dbl_45;
+ijk_ext dbl const dbl_deg2rad;
+ijk_ext dbl const dbl_rad2deg;
+
+
+// global constants for real type
+///
+#define real_zero					ijk_tokencat(ijk_real,_zero)
+#define real_eps					ijk_tokencat(ijk_real,_eps)
+#define real_halfeps				ijk_tokencat(ijk_real,_halfeps)
+#define real_1000th					ijk_tokencat(ijk_real,_1000th)
+#define real_100th					ijk_tokencat(ijk_real,_100th)
+#define real_10th					ijk_tokencat(ijk_real,_10th)
+#define real_9th					ijk_tokencat(ijk_real,_9th)
+#define real_8th					ijk_tokencat(ijk_real,_8th)
+#define real_7th					ijk_tokencat(ijk_real,_7th)
+#define real_6th					ijk_tokencat(ijk_real,_6th)
+#define real_5th					ijk_tokencat(ijk_real,_5th)
+#define real_quarter				ijk_tokencat(ijk_real,_quarter)
+#define real_third					ijk_tokencat(ijk_real,_third)
+#define real_half					ijk_tokencat(ijk_real,_half)
+#define real_2thirds				ijk_tokencat(ijk_real,_2thirds)
+#define real_3quarters				ijk_tokencat(ijk_real,_3quarters)
+#define real_one					ijk_tokencat(ijk_real,_one)
+#define real_4third					ijk_tokencat(ijk_real,_4third)
+#define real_3half					ijk_tokencat(ijk_real,_3half)
+#define real_two					ijk_tokencat(ijk_real,_two)
+#define real_three					ijk_tokencat(ijk_real,_three)
+#define real_four					ijk_tokencat(ijk_real,_four)
+#define real_five					ijk_tokencat(ijk_real,_five)
+#define real_six					ijk_tokencat(ijk_real,_six)
+#define real_seven					ijk_tokencat(ijk_real,_seven)
+#define real_eight					ijk_tokencat(ijk_real,_eight)
+#define real_nine					ijk_tokencat(ijk_real,_nine)
+#define real_ten					ijk_tokencat(ijk_real,_ten)
+#define real_hundred				ijk_tokencat(ijk_real,_hundred)
+#define real_thousand				ijk_tokencat(ijk_real,_thousand)
+#define real_sqrtthird				ijk_tokencat(ijk_real,_sqrtthird)
+#define real_sqrthalf				ijk_tokencat(ijk_real,_sqrthalf)
+#define real_sqrt2					ijk_tokencat(ijk_real,_sqrt2)
+#define real_sqrt3					ijk_tokencat(ijk_real,_sqrt3)
+#define real_2pi					ijk_tokencat(ijk_real,_2pi)
+#define real_pi						ijk_tokencat(ijk_real,_pi)
+#define real_halfpi					ijk_tokencat(ijk_real,_halfpi)
+#define real_thirdpi				ijk_tokencat(ijk_real,_thirdpi)
+#define real_quarterpi				ijk_tokencat(ijk_real,_quarterpi)
+#define real_360					ijk_tokencat(ijk_real,_360)
+#define real_180					ijk_tokencat(ijk_real,_180)
+#define real_90						ijk_tokencat(ijk_real,_90)
+#define real_60						ijk_tokencat(ijk_real,_60)
+#define real_45						ijk_tokencat(ijk_real,_45)
+#define real_deg2rad				ijk_tokencat(ijk_real,_deg2rad)
+#define real_rad2deg				ijk_tokencat(ijk_real,_rad2deg)
 
 
 // ijk_lerp
@@ -90,148 +240,6 @@ typedef ijk_real		real;
 // ijk_remap_safe
 //	Re-map/re-interpolate with division-by-zero safety.
 #define ijk_remap_safe(v0_new,v1_new,v0_orig,v1_orig,v_orig)	((v0_orig) != (v1_orig) ? ijk_remap(v0_new,v1_new,v0_orig,v1_orig,v_orig) : (v0_new))
-
-
-// global constants for double, float and real
-///
-#define dbl_zero					0.0
-#define flt_zero					ijk_tokencat(dbl_zero,f)
-#define real_zero					ijk_tokencat(ijk_real,_zero)
-#define dbl_1000th					0.001
-#define flt_1000th					ijk_tokencat(dbl_1000th,f)
-#define real_1000th					ijk_tokencat(ijk_real,_1000th)
-#define dbl_100th					0.01
-#define flt_100th					ijk_tokencat(dbl_100th,f)
-#define real_100th					ijk_tokencat(ijk_real,_100th)
-#define dbl_10th					0.1
-#define flt_10th					ijk_tokencat(dbl_10th,f)
-#define real_10th					ijk_tokencat(ijk_real,_10th)
-#define dbl_9th						0.11111111111111111111111111111111
-#define flt_9th						ijk_tokencat(dbl_9th,f)
-#define real_9th					ijk_tokencat(ijk_real,_9th)
-#define dbl_8th						0.125
-#define flt_8th						ijk_tokencat(dbl_8th,f)
-#define real_8th					ijk_tokencat(ijk_real,_8th)
-#define dbl_7th						0.14285714285714285714285714285714
-#define flt_7th						ijk_tokencat(dbl_7th,f)
-#define real_7th					ijk_tokencat(ijk_real,_7th)
-#define dbl_6th						0.16666666666666666666666666666667
-#define flt_6th						ijk_tokencat(dbl_6th,f)
-#define real_6th					ijk_tokencat(ijk_real,_6th)
-#define dbl_5th						0.2
-#define flt_5th						ijk_tokencat(dbl_5th,f)
-#define real_5th					ijk_tokencat(ijk_real,_5th)
-#define dbl_quarter					0.25
-#define flt_quarter					ijk_tokencat(dbl_quarter,f)
-#define real_quarter				ijk_tokencat(ijk_real,_quarter)
-#define dbl_third					0.33333333333333333333333333333333
-#define flt_third					ijk_tokencat(dbl_third,f)
-#define real_third					ijk_tokencat(ijk_real,_third)
-#define dbl_half					0.5
-#define flt_half					ijk_tokencat(dbl_half,f)
-#define real_half					ijk_tokencat(ijk_real,_half)
-#define dbl_2thirds					0.66666666666666666666666666666667
-#define flt_2thirds					ijk_tokencat(dbl_2thirds,f)
-#define real_2thirds				ijk_tokencat(ijk_real,_2thirds)
-#define dbl_3quarters				0.75
-#define flt_3quarters				ijk_tokencat(dbl_3quarters,f)
-#define real_3quarters				ijk_tokencat(ijk_real,_3quarters)
-#define dbl_one						1.0
-#define flt_one						ijk_tokencat(dbl_one,f)
-#define real_one					ijk_tokencat(ijk_real,_one)
-#define dbl_4third					1.3333333333333333333333333333333
-#define flt_4third					ijk_tokencat(dbl_4third,f)
-#define real_4third					ijk_tokencat(ijk_real,_4third)
-#define dbl_3half					1.5
-#define flt_3half					ijk_tokencat(dbl_3half,f)
-#define real_3half					ijk_tokencat(ijk_real,_3half)
-#define dbl_two						2.0
-#define flt_two						ijk_tokencat(dbl_two,f)
-#define real_two					ijk_tokencat(ijk_real,_two)
-#define dbl_three					3.0
-#define flt_three					ijk_tokencat(dbl_three,f)
-#define real_three					ijk_tokencat(ijk_real,_three)
-#define dbl_four					4.0
-#define flt_four					ijk_tokencat(dbl_four,f)
-#define real_four					ijk_tokencat(ijk_real,_four)
-#define dbl_five					5.0
-#define flt_five					ijk_tokencat(dbl_five,f)
-#define real_five					ijk_tokencat(ijk_real,_five)
-#define dbl_six						6.0
-#define flt_six						ijk_tokencat(dbl_six,f)
-#define real_six					ijk_tokencat(ijk_real,_six)
-#define dbl_seven					7.0
-#define flt_seven					ijk_tokencat(dbl_seven,f)
-#define real_seven					ijk_tokencat(ijk_real,_seven)
-#define dbl_eight					8.0
-#define flt_eight					ijk_tokencat(dbl_eight,f)
-#define real_eight					ijk_tokencat(ijk_real,_eight)
-#define dbl_nine					9.0
-#define flt_nine					ijk_tokencat(dbl_nine,f)
-#define real_nine					ijk_tokencat(ijk_real,_nine)
-#define dbl_ten						10.0
-#define flt_ten						ijk_tokencat(dbl_ten,f)
-#define real_ten					ijk_tokencat(ijk_real,_ten)
-#define dbl_hundred					100.0
-#define flt_hundred					ijk_tokencat(dbl_hundred,f)
-#define real_hundred				ijk_tokencat(ijk_real,_hundred)
-#define dbl_thousand				1000.0
-#define flt_thousand				ijk_tokencat(dbl_thousand,f)
-#define real_thousand				ijk_tokencat(ijk_real,_thousand)
-#define dbl_sqrtthird				0.57735026918962576450914878050196
-#define flt_sqrtthird				ijk_tokencat(dbl_sqrtthird,f)
-#define real_sqrtthird				ijk_tokencat(ijk_real,_sqrtthird)
-#define dbl_sqrthalf				0.70710678118654752440084436210485
-#define flt_sqrthalf				ijk_tokencat(dbl_sqrthalf,f)
-#define real_sqrthalf				ijk_tokencat(ijk_real,_sqrthalf)
-#define dbl_sqrt2					1.4142135623730950488016887242097
-#define flt_sqrt2					ijk_tokencat(dbl_sqrt2,f)
-#define real_sqrt2					ijk_tokencat(ijk_real,_sqrt2)
-#define dbl_sqrt3					1.7320508075688772935274463415059
-#define flt_sqrt3					ijk_tokencat(dbl_sqrt3,f)
-#define real_sqrt3					ijk_tokencat(ijk_real,_sqrt3)
-#define dbl_2pi						6.283185307179586476925286766559
-#define flt_2pi						ijk_tokencat(dbl_2pi,f)
-#define real_2pi					ijk_tokencat(ijk_real,_2pi)
-#define dbl_pi						3.1415926535897932384626433832795
-#define flt_pi						ijk_tokencat(dbl_pi,f)
-#define real_pi						ijk_tokencat(ijk_real,_pi)
-#define dbl_halfpi					1.5707963267948966192313216916398
-#define flt_halfpi					ijk_tokencat(dbl_halfpi,f)
-#define real_halfpi					ijk_tokencat(ijk_real,_halfpi)
-#define dbl_thirdpi					1.0471975511965977461542144610932
-#define flt_thirdpi					ijk_tokencat(dbl_thirdpi,f)
-#define real_thirdpi				ijk_tokencat(ijk_real,_thirdpi)
-#define dbl_quarterpi				0.78539816339744830961566084581988
-#define flt_quarterpi				ijk_tokencat(dbl_quarterpi,f)
-#define real_quarterpi				ijk_tokencat(ijk_real,_quarterpi)
-#define dbl_360						360.0
-#define flt_360						ijk_tokencat(dbl_360,f)
-#define real_360					ijk_tokencat(ijk_real,_360)
-#define dbl_180						180.0
-#define flt_180						ijk_tokencat(dbl_180,f)
-#define real_180					ijk_tokencat(ijk_real,_180)
-#define dbl_90						90.0
-#define flt_90						ijk_tokencat(dbl_90,f)
-#define real_90						ijk_tokencat(ijk_real,_90)
-#define dbl_60						60.0
-#define flt_60						ijk_tokencat(dbl_60,f)
-#define real_60						ijk_tokencat(ijk_real,_60)
-#define dbl_45						45.0
-#define flt_45						ijk_tokencat(dbl_45,f)
-#define real_45						ijk_tokencat(ijk_real,_45)
-#define dbl_deg2rad					0.01745329251994329576923690768489
-#define flt_deg2rad					ijk_tokencat(dbl_deg2rad,f)
-#define real_deg2rad				ijk_tokencat(ijk_real,_deg2rad)
-#define dbl_rad2deg					57.295779513082320876798154814105
-#define flt_rad2deg					ijk_tokencat(dbl_rad2deg,f)
-#define real_rad2deg				ijk_tokencat(ijk_real,_rad2deg)
-#define dbl_eps						2.22e-16
-#define flt_eps						1.19e-07f
-#define real_eps					ijk_tokencat(ijk_real,_eps)
-#define dbl_halfeps					1.11e-16
-#define flt_halfeps					5.96e-08f
-#define real_halfeps				ijk_tokencat(ijk_real,_halfeps)
 
 
 // sign function
