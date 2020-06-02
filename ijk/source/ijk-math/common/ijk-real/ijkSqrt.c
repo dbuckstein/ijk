13c18f857fe5864b6b88731cb3985f853d0e9125
dbuckstein <dbuckstein@users.noreply.github.com>
2020-06-01 22:56:43 -0400

    Fixing some code highlighting confusion by adding dummy sqrt source, excluded from build so that the header knows about definitions for all functions. MODIFIED: math project and filters, added file. ADDED: empty sqrt source.

---
+++ ijk/source/ijk-math/common/ijk-real/ijkSqrt.c
@@ -0,0 +1,59 @@
+/*
+   Copyright 2020 Daniel S. Buckstein
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+/*
+	ijk: an open-source, cross-platform, light-weight,
+		c-based rendering framework
+	By Daniel S. Buckstein
+
+	ijkSqrt.c
+	Source definitions for square root.
+*/
+
+#include "ijk/ijk-math/ijk-real/ijkSqrt.h"
+
+
+//-----------------------------------------------------------------------------
+
+flt ijkSqrt_flt(flt const x)
+{
+	// see 'ijkSqrt_a.asm'
+	return x;
+}
+
+
+dbl ijkSqrt_dbl(dbl const x)
+{
+	// see 'ijkSqrt_a.asm'
+	return x;
+}
+
+
+flt ijkSqrtInv_flt(flt const x)
+{
+	// see 'ijkSqrt_a.asm'
+	return x;
+}
+
+
+dbl ijkSqrtInv_dbl(dbl const x)
+{
+	// see 'ijkSqrt_a.asm'
+	return x;
+}
+
+
+//-----------------------------------------------------------------------------
