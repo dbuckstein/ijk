3d1cf6fa377c6b578f4ab122007f455b234b2be5
dbuckstein <dbuckstein@users.noreply.github.com>
2020-08-17 23:07:47 -0400

    Starting trigonometry. MODIFIED: math project and filters, added trig source to hold pointers and globals; main macros header, added looping clamp utility to help with trig param validation; trig header, started adding function documentation. ADDED: trig source.

--- ijk/include/ijk/ijk-math/ijk-real/ijkTrigonometry.h
+++ ijk/include/ijk/ijk-math/ijk-real/ijkTrigonometry.h
@@ -32,6 +32,57 @@
 
 //-----------------------------------------------------------------------------
 
+// ijkTrigGetTableSize_flt
+//	Get the minimum required fast trigonometry table size in bytes. This will 
+//	be different based on the sample type (float or double) and architecture 
+//	(32-bit or 64-bit integer for index).
+//		param subdivisionsPerDegree: number of samples per degree
+//			valid: non-zero
+//			note: each degree will be subsampled this number of times
+//		return SUCCESS: ijk_success if set data
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+
+
+// ijkTrigSetTable_flt
+//	Set data tables for fast trigonometry.
+//		param table: pointer to pre-allocated table
+//			valid: non-null
+//		param tableSize_bytes: size of provided table in bytes
+//			valid: at least the minimum required size
+//			note: minimum required size (in bytes) is: 
+//				SZ	= SZ_sample * (
+//						(720 degrees * 5 sets [params, sine, cosine, cosecant, secant]
+//						+ 90 degrees * 2 sets [cosine, secant])
+//						* (samples per degree) + 4 padding)
+//					+ SZ_index * (
+//						(720 * 2 sets[arcsine, arccosine])
+//						+ 4 padding)
+//		param subdivisionsPerDegree: number of samples per degree
+//			valid: non-zero
+//			note: each degree will be subsampled this number of times
+//		return SUCCESS: ijk_success if set data
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+
+
+// ijkTrigInit_flt
+//	Initialize and set data tables for fast trigonometry.
+//		param table_out: pointer to pre-allocated table to store data
+//			valid: non-null
+//		param tableSize_bytes: size of provided table in bytes
+//			valid: at least the minimum required size
+//			note: minimum required size (in bytes) is: 
+//				SZ	= SZ_sample * (
+//						(720 degrees * 5 sets [params, sine, cosine, cosecant, secant]
+//						+ 90 degrees * 2 sets [cosine, secant])
+//						* (samples per degree) + 4 padding)
+//					+ SZ_index * (
+//						(720 * 2 sets[arcsine, arccosine])
+//						+ 4 padding)
+//		param subdivisionsPerDegree: number of samples per degree
+//			valid: non-zero
+//			note: each degree will be subsampled this number of times
+//		return SUCCESS: ijk_success if initialized data
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
 
 
 //-----------------------------------------------------------------------------
