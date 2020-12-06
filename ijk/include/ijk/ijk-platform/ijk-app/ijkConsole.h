81623ac9c8d71b5750e510eee37407bfda464788
dbuckstein <dbuckstein@users.noreply.github.com>
2020-12-06 17:07:28 -0500

    Adding some console manipulation features. MOD: console header, added colors enum and prototypes.

--- ijk/include/ijk/ijk-platform/ijk-app/ijkConsole.h
+++ ijk/include/ijk/ijk-platform/ijk-app/ijkConsole.h
@@ -32,7 +32,8 @@
 #ifdef __cplusplus
 extern "C" {
 #else	// !__cplusplus
-typedef struct ijkConsole ijkConsole;
+typedef struct ijkConsole		ijkConsole;
+typedef enum ijkConsoleColor	ijkConsoleColor;
 #endif	// __cplusplus
 
 
@@ -52,6 +53,22 @@ struct ijkConsole
 {
 	ptr handle[4];
 	i32 io[3];
+};
+
+
+// ijkConsoleColor
+//	List of color codes for changing display style in console.
+enum ijkConsoleColor
+{
+	ijkConsoleColor_off = 0x0,		// Color is disabled (black).
+	ijkConsoleColor_blue = 0x1,		// Color contains blue channel.
+	ijkConsoleColor_green = 0x2,	// Color contains green channel.
+	ijkConsoleColor_cyan = ijkConsoleColor_green | ijkConsoleColor_blue,	// Color contains green and blue channels to form cyan.
+	ijkConsoleColor_red = 0x4,		// Color contains red channel.
+	ijkConsoleColor_magenta = ijkConsoleColor_blue | ijkConsoleColor_red,	// Color contains blue and red channels to form magenta.
+	ijkConsoleColor_yellow = ijkConsoleColor_red | ijkConsoleColor_green,	// Color contains red and green channels to form yellow.
+	ijkConsoleColor_white = ijkConsoleColor_red | ijkConsoleColor_green | ijkConsoleColor_blue,	// Color contains all primary channels.
+	ijkConsoleColor_alpha = 0x8		// Color is fully opaque/intense.
 };
 
 
@@ -88,6 +105,86 @@ iret ijkConsoleRedirectMain(ijkConsole* const console, ibool const redirectInput
 //		return FAILURE: ijk_fail_operationfail if console not terminated
 //		return FAILURE: ijk_fail_invalidparams if invalid parameters
 iret ijkConsoleReleaseMain(ijkConsole* const console);
+
+
+//-----------------------------------------------------------------------------
+
+// ijkConsoleGetCursor
+//	Get position of cursor in console.
+//		param x_out: pointer to value to store horizontal coordinate
+//			valid: non-null
+//		param y_out: pointer to value to store vertical coordinate (from top)
+//			valid: non-null
+//		return SUCCESS: ijk_success if operation succeeded
+//		return FAILURE: ijk_fail_operationfail if operation failed
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkConsoleGetCursor(i16* const x_out, i16* const y_out);
+
+// ijkConsoleSetCursor
+//	Set position of cursor in console.
+//		param x: value of horizontal coordinate to set
+//		param y: value of vertical coordinate to set (from top)
+//		return SUCCESS: ijk_success if operation succeeded
+//		return FAILURE: ijk_fail_operationfail if operation failed
+iret ijkConsoleSetCursor(i16 const x, i16 const y);
+
+// ijkConsoleToggleCursor
+//	Toggle blinking underscore at cursor location.
+//		param visible: visible flag; interpreted as boolean
+//		return SUCCESS: ijk_success if operation succeeded
+//		return FAILURE: ijk_fail_operationfail if operation failed
+iret ijkConsoleToggleCursor(ibool const visible);
+
+// ijkConsoleGetColor
+//	Set color of console text.
+//		param fg_out: pointer to description of foreground (character) channels
+//			valid: non-null
+//		param bg_out: pointer to description of background (console) channels
+//			valid: non-null
+//		return SUCCESS: ijk_success if operation succeeded
+//		return FAILURE: ijk_fail_operationfail if operation failed
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkConsoleGetColor(ijkConsoleColor* const fg_out, ijkConsoleColor* const bg_out);
+
+// ijkConsoleSetColor
+//	Set color of console text.
+//		param fg: description of foreground (character) channels to use
+//		param bg: description of background (console) channels to use
+//		return SUCCESS: ijk_success if operation succeeded
+//		return FAILURE: ijk_fail_operationfail if operation failed
+iret ijkConsoleSetColor(ijkConsoleColor const fg, ijkConsoleColor const bg);
+
+// ijkConsoleResetColor
+//	Reset color of console text.
+//		return SUCCESS: ijk_success if operation succeeded
+//		return FAILURE: ijk_fail_operationfail if operation failed
+iret ijkConsoleResetColor();
+
+// ijkConsoleGetCursorColor
+//	Get console cursor position and color.
+//		param x_out: pointer to value to store horizontal coordinate
+//			valid: non-null
+//		param y_out: pointer to value to store vertical coordinate (from top)
+//			valid: non-null
+//		param fg_out: pointer to description of foreground (character) channels
+//			valid: non-null
+//		param bg_out: pointer to description of background (console) channels
+//			valid: non-null
+//		return SUCCESS: ijk_success if operation succeeded
+//		return FAILURE: ijk_fail_operationfail if operation failed
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkConsoleGetCursorColor(i16* const x_out, i16* const y_out, ijkConsoleColor* const fg_out, ijkConsoleColor* const bg_out);
+
+// ijkConsoleSetCursorColor
+//	Get console cursor position and color.
+//		param x: value of horizontal coordinate to set
+//		param y: value of vertical coordinate to set (from top)
+//		param fg: description of foreground (character) channels to use
+//		param bg: description of background (console) channels to use
+//		return SUCCESS: ijk_success if operation succeeded
+//		return FAILURE: ijk_fail_operationfail if operation failed
+//		return FAILURE: ijk_fail_invalidparams if invalid parameters
+iret ijkConsoleSetCursorColor(i16 const x, i16 const y, ijkConsoleColor const fg, ijkConsoleColor const bg);
 
 
 //-----------------------------------------------------------------------------
